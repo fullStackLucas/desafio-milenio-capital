4efcf02be8f4adb79e7d537c3ef8ce6d667c2721
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-28 18:38:16 -0300

    modificacao docker-compose

--- server/config/config.js
+++ server/config/config.js
@@ -5,8 +5,9 @@ module.exports = {
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',
     database: 'milenio_capital',
-    host: process.env.HOSTNAME || 'db',
+    host: process.env.HOSTNAME || 'localhost',
     dialect: 'mysql',
+    port: 3308
   },
   test: {
     username: process.env.MYSQL_USER || 'root',
