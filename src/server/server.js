0833dc6ae66f1c1ce8a97fe29b4fb87fa36c08a5
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-28 14:45:26 -0300

    Implementando server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+
+app.listen(PORT, () => console.log(`Escutando a porta ${PORT}`))
