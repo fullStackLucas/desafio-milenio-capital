f12cb51985fcc5928c913a4245ffb9d618d5f5b5
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-28 17:39:33 -0300

    Cria model Rota

---
+++ server/models/route.js
@@ -0,0 +1,38 @@
+const { DataTypes, Model } = require('sequelize');
+const Graph = require('./graph');
+const db = require('.');
+
+const Attributes = {
+  graphId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  source: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  target: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  distance: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+};
+
+class Route extends Model {};
+
+Route.init(
+  Attributes,
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'route',
+    timestamps: false,
+  },
+);
+
+Route.belongsTo(Graph);
+
+module.exports = Graph;
