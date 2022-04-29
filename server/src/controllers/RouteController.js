c5fc13de4bdca2419140ceeefde5f34ea8e9b015
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-29 20:34:53 -0300

    controller da rota mais curta implementado

--- server/src/controllers/RouteController.js
+++ server/src/controllers/RouteController.js
@@ -8,6 +8,15 @@ const getAllById = async (req, res) => {
   return res.status(200).json(graphRoutes);
 }
 
+const getShortestPath = async (req, res) => {
+  const { graphId, town1, town2 } = req.params;
+  const shortestPath = await RouteService(graphId, town1, town2);
+
+  if(!shortestPath) return res.status(404).json({ error: 'NOT FOUND (404)' })
+  return res.status(200).json(shortestPath);
+}
+
 module.exports = {
-  getAllById
+  getAllById,
+  getShortestPath,
 }
