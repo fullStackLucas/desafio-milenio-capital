36dd9b6717ee78a424ee26d3c2856db23f48df41
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-29 17:10:15 -0300

    seeder das rotas implementado

--- server/src/seeders/20220429200536-routes.js
+++ server/src/seeders/20220429200536-routes.js
@@ -1,24 +1,20 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  up: async (queryInterface, _Sequelize) => queryInterface.bulkInsert('routes',
+    [
+      {id: 1, source: "A", target: "B", distance: 6},
+      {id: 1, source: "A", target: "E", distance: 4},
+      {id: 1, source: "B", target: "A", distance: 6},
+      {id: 1, source: "B", target: "C", distance: 2},
+      {id: 1, source: "B", target: "D", distance: 4},
+      {id: 1, source: "C", target: "B", distance: 3},
+      {id: 1, source: "C", target: "D", distance: 1},
+      {id: 1, source: "C", target: "E", distance: 7},
+      {id: 1, source: "D", target: "B", distance: 8},
+      {id: 1, source: "E", target: "B", distance: 5},
+      {id: 1, source: "E", target: "D", distance: 7},
+    ], {}),
+      
+  down: async (queryInterface) => queryInterface.bulkDelete('routes', null, {}),
 };
