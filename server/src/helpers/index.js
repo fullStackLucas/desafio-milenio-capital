fec81864e4477f194ff16215641c886188c3cbc9
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-29 20:06:58 -0300

    Função para transformar array em objetos implementada

---
+++ server/src/helpers/index.js
@@ -0,0 +1,19 @@
+const nodesObject = (data) => {
+  const newObjectNode = {};
+
+  data.forEach(({source, target, distance}) => {
+    let insideObject = {}
+    insideObject[target] = distance;
+    if(!newObjectNode[source]) {
+      newObjectNode[source] = insideObject;
+      insideObject = {}
+    } else {
+      newObjectNode[source] = {
+        ...newObjectNode[source],
+        ...insideObject,
+      }
+      insideObject = {}
+    }
+  })
+  return newObjectNode;
+}
