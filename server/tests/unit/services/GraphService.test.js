872cd3916927d4f67a22e3ddf04a61c2959fd3a8
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-30 19:15:00 -0300

    Estrutura do teste do endpoint POST /graph criada

--- server/tests/unit/services/GraphService.test.js
+++ server/tests/unit/services/GraphService.test.js
@@ -6,3 +6,27 @@ const {
   createGraphMock,
   bulkCreateRouteMock,
 } = require('../../mocks');
+
+const GraphModel = require('../../../src/models/Graph');
+const RouteModel = require('../../../src/models/Route');
+const GraphService = require('../../../src/services/GraphsService');
+
+describe('GraphService', () => {
+  describe('POST /graph endpoint create functionality', () => {
+    beforeEach(() => {
+      const expectedResult = {
+        id: 2,
+        data: [...dataMock],
+      };
+      const graphMockResolved = createGraphMock();
+      const routeMockResolved = bulkCreateRouteMock();
+      sinon.stub(GraphModel, 'create').resolves(graphMockResolved);
+      sinon.stub(RouteModel, 'bulkCreate').resolves(routeMockResolved);
+    })
+
+    afterEach(() => {
+      GraphModel.create.restore();
+      RouteModel.bulkCreate.restore();
+    })
+  })
+})
