982a2ba4c14d555c097389cfd7c5d25559249d7a
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-30 18:37:25 -0300

    mockagem do array que será usado para testes completa

---
+++ server/tests/unit/mocks/index.js
@@ -0,0 +1,14 @@
+const dataMock = [
+  { source: "A", target: "B", distance: 6 },
+  { source: "A", target: "E", distance: 4 },
+  { source: "B", target: "A", distance: 6 },
+  { source: "B", target: "C", distance: 2 },
+  { source: "B", target: "D", distance: 4 },
+  { source: "C", target: "B", distance: 3 },
+  { source: "C", target: "D", distance: 1 },
+  { source: "C", target: "E", distance: 7 },
+  { source: "D", target: "B", distance: 8 },
+  { source: "E", target: "B", distance: 5 },
+  { source: "E", target: "D", distance: 7 }
+];
+
