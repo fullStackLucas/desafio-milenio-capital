0cda16f2fe9b52dda06479b25cb3230e7efd45f6
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-30 21:26:21 -0300

    uso do middleware de erro na aplicação

--- server/src/server.js
+++ server/src/server.js
@@ -6,10 +6,12 @@ const routes = require('./routes');
 
 const server = express();
 const PORT = process.env.PORT || 8080;
+const errorMiddleware = require('../src/moddlewares/errorMiddleware');
 
 server.use(json());
 server.use(routes);
 server.use(cors());
+server.use(errorMiddleware);
 
 
 server.listen(PORT, () => console.log(`Escutando a porta ${PORT}`))
