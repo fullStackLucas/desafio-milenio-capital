e2afb1c0f607df0e7e9314303ba23745a44414f6
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-04-30 21:01:15 -0300

    Test: Graph controller importações

---
+++ server/tests/unit/controllers/GraphController.test.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+const sinon = require('sinon');
+
+const GraphController = require('../../../src/controllers/GraphController');
+const GraphService = require('../../../src/services/GraphsService');
