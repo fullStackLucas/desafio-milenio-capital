90e0734bddfe64df6664f40cb75215ae39f30df0
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 14:50:00 -0300

    Adiciona middleware de validação nas rotas

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const RouteController = require('../controllers/RouteController');
 const GraphController = require('../controllers/GraphController');
+const validationMiddleware = require('../middlewares/validations');
 
 const routes = express.Router();
-routes.get('/graph/:graphId', RouteController.getAllById);
+routes.get('/graph/:graphId', validationMiddleware, RouteController.getAllById);
 routes.post('/graph', GraphController.create);
-routes.post('/distance/:graphId/from/:town1/to/:town2', RouteController.getShortestPath)
+routes.post(
+  '/distance/:graphId/from/:town1/to/:town2',
+  validationMiddleware,
+  RouteController.getShortestPath,
+);
 
 module.exports = routes;
