29b32c3bb91e769bf494385ab0ece4fbae67b08f
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 15:12:46 -0300

    Mensagens de validação modificadas

--- server/src/schemas/validateData.js
+++ server/src/schemas/validateData.js
@@ -4,20 +4,20 @@ module.exports = Joi.object({
   data: Joi.array().items(
     Joi.object({
       source: Joi.string().min(1).required().messages({
-        'any.required': '400|{{#label}} is required',
-        'string.empty': '400|{{#label}} is required',
-        'string.base': '422|{{#label}} must be a string',
-        'string.min': '422|{{#label}} must be at least 1 character long',
+        'any.required': '400|source at {{#label}} is required',
+        'string.empty': '400|source at {{#label}} is required',
+        'string.base': '422|source at {{#label}} must be a string',
+        'string.min': '422|source at {{#label}} must be at least 1 character long',
       }),
       target: Joi.string().min(1).required().messages({
-        'any.required': '400|{{#label}} is required',
-        'string.empty': '400|{{#label}} is required',
-        'string.base': '422|{{#label}} must be a string',
-        'string.min': '422|{{#label}} must be at least 1 character long',
+        'any.required': '400|target at {{#label}} is required',
+        'string.empty': '400|target at {{#label}} is required',
+        'string.base': '422|target at {{#label}} must be a string',
+        'string.min': '422|target at {{#label}} must be at least 1 character long',
       }),
       distance: Joi.number().required().messages({
-        'any.required': '400|{{#label}} is required',
-        'number.base': '422|{{#label}} must be a valid date',
+        'any.required': '400|distance at {{#label}} is required',
+        'number.base': '422|distance at {{#label}} must be a number',
       }),
     })
   )
