6e3338c98a091efcc368e40dc78c2f2833e809d9
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 19:00:35 -0300

    Configura server para ser usado tanto nos testes quanto para ser "escutado"

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { json } = require('express');
 const cors = require('cors');
 const routes = require('./routes');
@@ -6,11 +7,10 @@ const routes = require('./routes');
 const server = express();
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(json());
 server.use(routes);
 server.use(cors());
 server.use(errorMiddleware);
 
 module.exports = server;
-
-
