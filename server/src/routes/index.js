a73935765d37c88e98bd4053e216dc4d0ab79e43
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 14:59:16 -0300

    Ajusta validações de rotas

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,12 +4,8 @@ const GraphController = require('../controllers/GraphController');
 const validationMiddleware = require('../middlewares/validations');
 
 const routes = express.Router();
-routes.get('/graph/:graphId', validationMiddleware, RouteController.getAllById);
-routes.post('/graph', GraphController.create);
-routes.post(
-  '/distance/:graphId/from/:town1/to/:town2',
-  validationMiddleware,
-  RouteController.getShortestPath,
-);
+routes.get('/graph/:graphId', RouteController.getAllById);
+routes.post('/graph', validationMiddleware, GraphController.create);
+routes.post('/distance/:graphId/from/:town1/to/:town2', RouteController.getShortestPath);
 
 module.exports = routes;
