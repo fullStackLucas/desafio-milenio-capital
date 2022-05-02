8d8882d3b7e1c5ac5b359f82b260c2208959c917
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 14:44:56 -0300

    Implementa middleware de validações

---
+++ server/src/middlewares/validations.js
@@ -0,0 +1,14 @@
+const validatioMiddleware = require('../schemas/validateData');
+
+module.exports = (req, res, next) => {
+  const { error } = validatioMiddleware.validate(req.body);
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+    console.error({ code, message });
+
+    return res.status(code).json({ message });
+  }
+
+  next();
+};
