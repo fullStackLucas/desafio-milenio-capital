612facc04b96cfb14279d737fc7d61d88a18d3ad
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-02 22:30:28 -0300

    Implementando documentação com swagger

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const { json } = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 
 const server = express();
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(json());
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use(routes);
 server.use(cors());
 server.use(errorMiddleware);
