ee34e1dfde5405fc7d58f63729e40c640ee73a7e
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-05-03 18:35:01 -0300

    Retira comentários da configuração do sequelize

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,16 +3,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const config = require('../config/config.js');
 
-// const db = new Sequelize(
-//   config.database,
-//   config.username,
-//   config.password,
-//   {
-//     dialect: config.dialect,
-//     host: config.host,
-//     port: process.env.MYSQL_PORT
-//   }
-// );
 const db = new Sequelize(config);
 
 module.exports = db;
